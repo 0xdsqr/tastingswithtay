f13f52f90d228f64fb93a3c408b48058ac4606d9
0xdsqr <me@dsqr.dev>
2026-07-23 10:38:50 -0500

    feat(helm): trust the homelab database CA

--- nix/checks/helm.nix
+++ nix/checks/helm.nix
@@ -25,6 +25,16 @@ stdenvNoCC.mkDerivation {
       helm lint "$chart"
       helm template "$(basename "$chart")" "$chart" --namespace twt >/dev/null
       helm template "$(basename "$chart")" "$chart" --namespace twt -f "$chart/values-prod.yaml" >/dev/null
+
+      trustedCaRender="$(mktemp)"
+      helm template "$(basename "$chart")" "$chart" \
+        --namespace twt \
+        --set additionalTrustedCa.enabled=true \
+        --set additionalTrustedCa.configMapName=dsqr-home-root-ca \
+        >"$trustedCaRender"
+      grep -F 'name: NODE_EXTRA_CA_CERTS' "$trustedCaRender" >/dev/null
+      grep -F 'value: "/etc/dsqr/pki/dsqr-home-root-ca.pem"' "$trustedCaRender" >/dev/null
+      grep -F 'name: dsqr-home-root-ca' "$trustedCaRender" >/dev/null
     done
 
     mkdir -p "$out"
